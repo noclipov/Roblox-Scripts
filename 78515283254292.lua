3a73912014797324a3f861a192f0c3d0af7c5b85
dimanoclip <gamefolover2019@gmail.com>
2026-09-07 21:33:20 +0300

    1

--- 78515283254292.lua
+++ 78515283254292.lua
@@ -19,12 +19,12 @@ local function check_in(window_number)
     fireproximityprompt(workspace.Misc.CheckIn.Printer.PP); task.wait(0.6)
     local badge = base_check_in(window_number, true):WaitForChild("PrintedBadge")
     repeat task.wait() until badge.PP.Enabled
-    local name = base_check_in(window_number, true):WaitForChild("PrintedBadge").Tag.UI.Label.Text
+    local name = badge.Tag.UI.Label.Text
     if workspace.NPCs[name]:GetAttribute("Skinwalker") then
 		msg.Mini("Crimson", ("%s (#%s) is an ANOMALY!"):format(name, window_number), 10)
         return
     end
-    fireproximityprompt(base_check_in(window_number, true).PrintedBadge:WaitForChild("PP"))
+    fireproximityprompt(badge:WaitForChild("PP"))
     task.wait(0.1)
     fireproximityprompt(workspace.NPCs[name].PP)
 end
