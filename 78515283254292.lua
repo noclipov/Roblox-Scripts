00606d8d442cec63409818d56882233462aea360
dimanoclip <gamefolover2019@gmail.com>
2026-09-07 20:35:46 +0300

    1

--- 78515283254292.lua
+++ 78515283254292.lua
@@ -6,22 +6,28 @@ local function detected_anomaly(npc_name)
     msg.Mini("Crimson", ("%s is an ANOMALY!"):format(npc_name), 10)
     fireproximityprompt(workspace.Misc.ShutterButton.PP)
 end
-local function base_check_in(window_number)
+local function base_check_in(window_number, return_object)
 	window_number = window_number or 1
-    fireproximityprompt(workspace.Misc["CheckIn"..(window_number==1 and "" or window_number)]:WaitForChild("Form").PP)
-    fireproximityprompt(workspace.Misc["CheckIn"..(window_number==1 and "" or window_number)].Camera.PP)
+	return_object = return_object or false
+	if not return_object then
+		fireproximityprompt(workspace.Misc["CheckIn"..(window_number==1 and "" or window_number)]:WaitForChild("Form").PP)
+		fireproximityprompt(workspace.Misc["CheckIn"..(window_number==1 and "" or window_number)].Camera.PP)
+		return false
+	else
+		return workspace.Misc["CheckIn"..(window_number==1 and "" or window_number)]
+	end
 end
 local function check_in(window_number)
     window_number = window_number or 1
-    if window_number == 1 then base_check_in(window_number) else base_check_in(window_number) end
+    base_check_in(window_number)
     task.wait(0.5)
     fireproximityprompt(workspace.Misc.CheckIn.Computer.PP)
     task.wait(0.1)
     fireproximityprompt(workspace.Misc.CheckIn.Printer.PP)
     task.wait(0.6)
-    local badge = workspace.Misc["CheckIn"..(window_number == 2 and window_number or "")]:WaitForChild("PrintedBadge")
+    local badge = base_check_in(nil, true):WaitForChild("PrintedBadge")
     repeat task.wait() until badge.PP.Enabled
-    local name = workspace.Misc["CheckIn"..(window_number == 2 and window_number or "")].PrintedBadge.Tag.UI.Label.Text
+    local name = base_check_in(nil, true).PrintedBadge.Tag.UI.Label.Text
     if workspace.NPCs[name]:GetAttribute("Skinwalker") then
         detected_anomaly(name)
         return
