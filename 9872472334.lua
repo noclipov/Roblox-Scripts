cc037d97fbdf1dba546eb44b0892e383eefcd40d
dimanoclip <gamefolover2019@gmail.com>
2026-09-09 18:52:08 +0300

    1

--- 9872472334.lua
+++ 9872472334.lua
@@ -1,29 +1,59 @@
-local int = game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Character"):WaitForChild("Interact")
+-- local int = game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Character"):WaitForChild("Interact")
+-- local add = loadstring(game:HttpGet("https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/Libs/additional.lua"))()
+-- add.KeyBinds({
+-- 	F1 = function()
+-- 		for i,v in pairs(game.Players:GetChildren()) do
+-- 			if not v.Character or v == game.Players.LocalPlayer then continue end
+-- 			if v.Character:GetAttribute("Downed") and not v.Character:GetAttribute("Carried") then
+-- 				local lastpos = add.tp(v.Character.PrimaryPart.CFrame);task.wait(0.2)
+-- 				int:FireServer("Revive", true, v.Name);task.wait(0.2)
+-- 				add.tp(lastpos);task.wait(0.2)
+-- 			end
+-- 		end
+-- 	end,
+-- 	F2 = function()
+-- 		for i,v in pairs(game.Players:GetChildren()) do
+-- 			if not v.Character or v == game.Players.LocalPlayer then continue end
+-- 			if v.Character:GetAttribute("Downed") and not v.Character:GetAttribute("Carried") then
+-- 				local lastpos = add.tp(v.Character.PrimaryPart.CFrame);task.wait(0.2)
+-- 				int:FireServer("Carry", nil, v.Name);task.wait(0.1)
+-- 				add.tp(lastpos)
+-- 				break
+-- 			end
+-- 		end
+-- 	end,
+-- 	F3 = function()
+-- 		local zones = workspace.Game.Map.SafeZones:GetChildren()
+-- 		add.tp(zones[math.random(#zones)].CFrame)
+-- 	end,
+-- })
 local add = loadstring(game:HttpGet("https://raw.githubusercontent.com/noclipov/Roblox-Luas/main/Libs/additional.lua"))()
-add.KeyBinds({
-	F1 = function()
-		for i,v in pairs(game.Players:GetChildren()) do
-			if not v.Character or v == game.Players.LocalPlayer then continue end
-			if v.Character:GetAttribute("Downed") and not v.Character:GetAttribute("Carried") then
-				local lastpos = add.tp(v.Character.PrimaryPart.CFrame);task.wait(0.2)
-				int:FireServer("Revive", true, v.Name);task.wait(0.2)
-				add.tp(lastpos);task.wait(0.2)
-			end
-		end
-	end,
-	F2 = function()
-		for i,v in pairs(game.Players:GetChildren()) do
-			if not v.Character or v == game.Players.LocalPlayer then continue end
-			if v.Character:GetAttribute("Downed") and not v.Character:GetAttribute("Carried") then
-				local lastpos = add.tp(v.Character.PrimaryPart.CFrame);task.wait(0.2)
-				int:FireServer("Carry", nil, v.Name);task.wait(0.1)
-				add.tp(lastpos)
-				break
-			end
-		end
-	end,
-	F3 = function()
-		local zones = workspace.Game.Map.SafeZones:GetChildren()
-		add.tp(zones[math.random(#zones)].CFrame)
-	end,
-})
+local msg = add.module("notify.lua")
+local UserInputService = game:GetService("UserInputService")
+local lp = game.Players.LocalPlayer
+local hum = lp.Character and lp.Character.Humanoid or lp.CharacterAdded:Wait() and lp.Character.Humanoid
+local function setup_bhop(humanoid)
+    hum.StateChanged:Connect(function(_, newState)
+        if newState == Enum.HumanoidStateType.Landed and IsHoldingJump then
+            hum:ChangeState(Enum.HumanoidStateType.Jumping)
+        end
+    end)
+    msg.Mini("Mint", "Enabled aut0-bhop.", 5)
+end
+setup_bhop(hum)
+local IsHoldingJump = false
+lp.CharacterAdded:Connect(function(char)
+    local hum = char:WaitForChild("Humanoid")
+    setup_bhop(hum)
+end)
+UserInputService.InputBegan:Connect(function(input, gpe)
+    if gpe then return end
+    if input.KeyCode == Enum.KeyCode.Space then
+        IsHoldingJump = true
+    end
+end)
+UserInputService.InputEnded:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.Space then
+        IsHoldingJump = false
+    end
+end)
